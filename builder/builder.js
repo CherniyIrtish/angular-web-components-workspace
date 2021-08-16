6a344ae4503c1e23e053317c584123a7069e89f8
cherniyirtish <komendantov.aleksii@gmail.com>
2021-08-16 22:35:44 +0200

    refresh README

--- builder/builder.js
+++ builder/builder.js
@@ -1,12 +1,12 @@
 const fileSystem = require('fs');
-const getEncapsulatedCss = require('./get-encapsulated-css.js');
 const concatJsBundles = require('./concat-js-bundles.js');
+const getEncapsulatedCss = require('./get-encapsulated-css.js');
 const mergeToJs = require('./merge-to-js');
 
 const bundleName = process.argv[2];
 
 const buildWebComponent = () => {
-  fileSystem.readdir(`dist/${bundleName}`, async (err, projects) => {
+  fileSystem.readdir(`dist/${bundleName}`, async (err) => {
     if (!err) {
       await concatJsBundles(bundleName);
       const encapsulatedCss = await getEncapsulatedCss(bundleName);
